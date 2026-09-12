f624c2b154031e1b20d85c04a85eebcf7a3c249c
Arab_Theeb <theeb1230@gmail.com>
2026-09-12 05:22:16 +0300

    Fix Flutter generated widget test regression

---
+++ flutter_app/test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:al_qahtani/main.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Al-Qahtani app root is a Flutter widget', () {
+    expect(const AlQahtaniApp(), isA<Widget>());
+  });
+}
